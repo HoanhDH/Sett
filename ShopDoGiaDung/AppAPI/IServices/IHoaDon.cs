c6c1ff80af0eecd1248bd9d62f5044457dedc3e7
Hoành <101155961+HoanhDH@users.noreply.github.com>
2023-05-27 08:32:37 +0700

    ok hoa don

---
+++ ShopDoGiaDung/AppAPI/IServices/IHoaDon.cs
@@ -0,0 +1,14 @@
+﻿using ShopDoGiaDung.Models;
+
+namespace AppAPI.IServices
+{
+    public interface IHoaDon
+    {
+        public bool AddHoaDon(HoaDon hoadon);
+        public bool UpdateHoaDon(HoaDon hoadon);
+        public bool DeleteHoaDon(Guid id);
+        public List<HoaDon> GetAllHoaDon();
+        public HoaDon GetBillById(Guid id);
+        //public List<Bill> GetBillByName(string name);
+    }
+}
